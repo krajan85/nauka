f83c537b7be56164fd983c1226ccb21cd4e1d53d
szczepanski.mi <michal.szczepanski@b3group.pl>
2024-02-22 21:41:17 +0100

    pliki do nauki

---
+++ script.js
@@ -0,0 +1,21 @@
+x = 10;
+console.log(x);
+x = x + 2;
+console.log(x);
+x = x - 3;
+console.log(x);
+x = x / 3;
+console.log(x);
+x = x * 3;
+console.log(x);
+
+rokUrodzenia = 1985;
+miesiac = 7;
+console.log(rokUrodzenia+miesiac);
+x = 8;
+y = 12;
+z = 15;
+imie='kamil'
+kamil='kamil';
+wynik = kamil + y;
+console.log(wynik);
